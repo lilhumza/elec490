bd87c66556dde8986af0f7bdfc13340e5f07330a
Darien Gaw <61702748+fishyjager@users.noreply.github.com>
2026-03-04 20:59:57 -0500

    Integrate motor control API into Launch.sh
    Added motor control API startup and cleanup handling.

--- Launch.sh
+++ Launch.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# Start MJPG‑Streamer
+# Start motor control API (thrusters + actuator)
+python3 motor_system/motor.py &
+MOTOR_PID=$!
+
+# Start MJPG-Streamer
 mjpg_streamer -o "output_http.so -p 5050 -w /home/subrov/mjpg-streamer-experimental/www" \
               -i "input_uvc.so -d /dev/video0 -r 1920x1080 -f 30" &
+MJPG_PID=$!
 
-# Give mjpg_streamer a moment to start, then start Node server
+# Give background services a moment to start, then start Node server
 sleep 1
+
+cleanup() {
+  kill "$MOTOR_PID" "$MJPG_PID" 2>/dev/null
+}
+
+trap cleanup EXIT
 node server.js
